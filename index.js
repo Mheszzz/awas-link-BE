e458f2dac504934d82ef92c115a3cc65cf7b22a8
Mahesa Rahmad Ramdani <mahesarahmad7@student.uns.ac.id>
2026-06-01 22:37:33 +0700

    Done Project Deployment

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
 const app = require('./src/app');
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+// Blok ini hanya akan berjalan saat kamu mengetik 'npm run dev' di lokal
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`🚀 Server lokal berjalan di http://localhost:${port}`);
+    });
+}
 
-// Menyalakan Server
-app.listen(PORT, () => {
-    console.log(`======================================`);
-    console.log(`🚀 Back-End AwasLink berhasil menyala!`);
-    console.log(`📡 Server berjalan di http://localhost:${PORT}`);
-    console.log(`======================================`);
-});
+// Wajib mengekspor modul 'app' agar Vercel bisa membacanya
+module.exports = app;
